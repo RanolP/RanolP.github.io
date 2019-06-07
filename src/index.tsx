f72ddbf8e9135c851e4d70b3dd4fff0688fc8253
RanolP <public.ranolp@gmail.com>
2019-06-07 18:18:14 +0900

    Unregister service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './pages/index';
+
 import './styles/default';
 import './styles/font';
 import './styles/reset';
+
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <>
@@ -11,3 +14,5 @@ ReactDOM.render(
   </>,
   document.getElementById('root')
 );
+
+serviceWorker.unregister();
